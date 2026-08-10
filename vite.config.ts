e09a2df6d601d9359d2a0ed027adde4ac5df2ed8
Raheem Kawojue <kawojue08@gmail.com>
2026-08-11 00:19:11 +0100

    [update] add nitro package to dependencies and update Vite configuration to include nitro plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import { devtools } from "@tanstack/devtools-vite";
-
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
-
+import { nitro } from "nitro/vite";
 import viteReact from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
@@ -11,7 +10,7 @@ const config = defineConfig({
     server: {
         allowedHosts: ["kawojue.dev"],
     },
-    plugins: [devtools(), tailwindcss(), tanstackStart(), viteReact()],
+    plugins: [devtools(), tailwindcss(), tanstackStart(), nitro(), viteReact()],
 });
 
 export default config;
